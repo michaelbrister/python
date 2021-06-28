3d9f5e2d121de5a1b15add36d0d75bfafb50959f
Michael Brister <mike@mikebrister.com>
2021-06-28 08:02:16 -0400

    Create ec2-decrypt-pass.py
    Use AWS boto3 to get and decrypt an Windows ec2 instance password.

---
+++ aws/ec2-decrypt-pass.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3 
+
+import base64
+import boto3
+import rsa
+
+instance_id = ''
+profile = ''
+region = ''
+env = ''
+key_path = ''
+
+def decrypt_ec2_pass(instance):
+    # Get AWS credentials
+    session = boto3.Session(profile_name=profile)
+    credentials = session.get_credentials()
+
+    ec2 = session.client('ec2', region_name=region)
+    instance = ec2.get_password_data(InstanceId=instance)
+
+    ecrypted_pass = base64.b64decode(instance['PasswordData'])
+
+    if (ecrypted_pass):
+        with open (key_path,'r') as privkeyfile:
+            priv = rsa.PrivateKey.load_pkcs1(privkeyfile.read())
+        key = rsa.decrypt(ecrypted_pass,priv)
+    else:
+        key = 'Wait at least 4 minutes after creation before the admin password is available'
+    return key.decode('utf-8')
+ 
+
+if __name__ == "__main__":
+    password = decrypt_ec2_pass(instance_id)
+    print(password)
