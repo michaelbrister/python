7be9cdd35908bc083e6e22ade5511a3c546de77d
Michael Brister <mike@mikebrister.com>
2022-11-01 11:09:20 -0400

    Create s3ListObjects.py

---
+++ aws/s3ListObjects.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import boto3
+
+def s3List(env, s3Bucket):
+    """Looks up an IP in the AWS tag list  based on the name passed."""
+    # Grab the key for the proper environment.
+    session = boto3.Session(profile_name=env, region_name="us-east-1")
+
+    # Get all instances for this account.
+    s3 = session.resource('s3')
+    my_bucket = s3.Bucket(s3Bucket)
+
+    for my_bucket_object in my_bucket.objects.all():
+        print(my_bucket_object.key)
+
+if __name__ == "__main__":
+
+    env = ""
+    s3Bucket = ""
+    s3List(env, s3Bucket)
